9a6cccf7b9281cfce6604684a11011637418470f
Jayasoorya K Deepak <jayasooryakdeepak@gmail.com>
2024-03-07 19:07:27 +0530

    Unwanted imports removed

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,9 +1,5 @@
-import 'dart:ffi';
-
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:gst_calculator/main.dart';
 import 'package:page_transition/page_transition.dart';
 
